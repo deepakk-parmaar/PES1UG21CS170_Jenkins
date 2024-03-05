aa792523d7b22ccf4c44d16123d6b19b7ba8a94a
deepakParmar <dpk2k2@gmail.com>
2024-03-05 15:19:32 +0530

    Initial Commit

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PES1UG21CS170\n";
+
+  return 0;
+}
